2deb1c92848c22dec190330b9a60e726f3cab4cf
Kevan Lee <kevanlee@users.noreply.github.com>
2014-11-08 14:07:49 -0700

    404 error page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+	<div id="content">
+
+	<h2><a href="/"><b>Page lost, a haiku</b></h2>
+<br>
+<br>
+our search is lonely<br>
+a footprint left in pure snow<br>
+blown into <a href="<?php echo get_option('home'); ?>/">nothing</a>
+	</div>
+
+<?php get_footer(); ?>
