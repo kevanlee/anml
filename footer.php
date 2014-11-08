e444cde75b97c4308569130258baabba1bff866d
Kevan Lee <kevanlee@users.noreply.github.com>
2014-11-08 14:07:25 -0700

    footer - links and nav

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+<div id="footer">
+
+	<p>
+	<a href="<?php bloginfo('url'); ?>/archives">more</a> :: <a href="http://mnmlist.com/uncopyright/">uncopyright</a> :: <a href="http://mnmlist.com/feeds/">feed</a> :: <a href="/less/">less</a>
+	</p>
+</div>
+</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</div>
+<a href="#0" class="cd-top">Top</a>
+</body>
+</html>
